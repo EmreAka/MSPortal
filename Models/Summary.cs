d67e7db6eb17c449b0b5f01996a9925496f11184
Emre <aka548548@gmail.com>
2023-03-11 03:10:04 +0300

    modal

---
+++ Models/Summary.cs
@@ -0,0 +1,11 @@
+﻿using Azure.AI.TextAnalytics;
+
+namespace BlazorSample.Models;
+
+public class Summary
+{
+    public DetectedLanguage DetectedLanguage{ get; set; }
+    public KeyPhraseCollection KeyPhraseCollection { get; set; }
+    public List<SummarySentence> SummarySentence { get; set; } = new();
+    public DocumentSentiment DocumentSentiment { get; set; }
+}
